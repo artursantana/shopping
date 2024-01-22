31c0246d19ad60bab466a91a14c2565a35bd0e4e
artursantana <artursantana843@gmail.com>
2024-01-22 22:47:04 +0000

    motando components de api e usando

--- src/pages/promotion/search/Search.tsx
+++ src/pages/promotion/search/Search.tsx
@@ -1,10 +1,42 @@
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+type Props = {
+  id: string
+  title: string
+}
+
+
+
 
 const Search = () => {
+
+  const [promo, setPromo] = useState<Props[]>([])
+
+ useEffect(()=> {
+
+  fetch('http://localhost:5000/promotions?_embed=comments').then((res)=>{
+    if(!res){
+      throw new Error("Network response was not ok");
+    }
+    return res.json()
+  }).then((data)=>{
+    setPromo(data)
+    console.log(data)
+  }).catch((error)=> {
+    console.log('Error fetching data:', error)
+  })
+
+ },[])
+
+
+
   return (
     <div>
-      Search
+      <p>feth</p>
+      <div>{promo.map((item)=>(
+        <p key={item.id}>{item.title}</p>
+      ))}</div>
     </div>
   )
 }
